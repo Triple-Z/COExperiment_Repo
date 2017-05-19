43ca1fa59e6195d9e469249de1942097b89aa2f8
TripleZ <me@triplez.cn>
2017-05-20 02:45:11 +0800

    Add(/datapath/dm.v): 4k Data Memory Module.
    - Update README.md

--- Project_1/datapath/dm.v
+++ Project_1/datapath/dm.v
@@ -1,8 +1,19 @@
-module dm_4k (addr, din, we, clk, dout);
-	input[11:2] addr;// address bus
-	input[31:0] din;// 32-bit input data
-	input we;// memory write enable
-	input clk;// clock
-	output[31:0] dout;  // 32-bit memory output
+module dm_4k (addr, din, wEn, clk, dout);
+	input 		[11:2] 	addr;
+	input 		[31:0] 	din;
+	input  			wEn;
+	input 			clk;
+	output 	reg 	[31:0] 	dout;
 
-endmodule // dm_4k
+	reg [31:0] dm [1023:0];// 32-bit*1024;
+
+	always @ ( addr or din ) begin// Read;
+		dout = dm[addr[11:2]][31:0];
+	end
+
+	always @ ( negedge clk ) begin// Write;
+		if (wEn) begin
+			dm[addr[11:2]][31:0] <= din[31:0];
+		end
+	end
+endmodule // 4K Data Memeory;
--- README.md
+++ README.md
@@ -34,7 +34,7 @@
 - [x] [ALU (Arithmetic Logic Unit)](Project_1/datapath/alu.v)
 - [x] [EXT (Extended Unit)](Project_1/datapath/ext.v)
 - [x] [IM (Instruction Memory)](Project_1/datapath/im.v) `4KB (32bits*1024)`
-- [ ] [DM (Data Memeory)](Project_1/datapath/dm.v) `4KB(32bits*1024)`
+- [x] [DM (Data Memeory)](Project_1/datapath/dm.v) `4KB(32bits*1024)`
 
 ### Control Module
 
