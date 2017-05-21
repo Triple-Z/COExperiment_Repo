8ff83212c805799e40904bab3f803b437eac2514
TripleZ <me@triplez.cn>
2017-05-21 17:12:36 +0800

    Add(/control/ctrl.v): Instruction Control Module.
    - Specify instruction control details in README.md

--- Project_1/datapath/im.v
+++ Project_1/datapath/im.v
@@ -1,6 +1,6 @@
-module im_4k (addr, dout);
+module im_4k (addr, ins);
 	input 	[11:2] addr;
-	output [31:0] dout;
+	output [31:0] ins;
 
 	reg	[31:0]	im[1023:0];// 32-bit*1024;
 
@@ -8,7 +8,7 @@ module im_4k (addr, dout);
 		$readmemh("code.txt", im);
 	end
 
-	assign dout = im[addr[11:2]][31:0];
+	assign ins = im[addr[11:2]][31:0];
 
 
 endmodule // 4k Instruction Memeory;
