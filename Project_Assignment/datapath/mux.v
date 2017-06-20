0d746984e499b01553b40789a239282cbeda3606
TripleZ <me@triplez.cn>
2017-06-20 15:13:02 +0800

    Modiy(PA/datapath/mux): change mulitiplxer to four ports.
    Update (README): control signal to 2 bits.

--- Project_Assignment/datapath/mux.v
+++ Project_Assignment/datapath/mux.v
@@ -1,10 +1,20 @@
 module mux #(parameter WIDTH = 32) (a, b, ctrl_s, dout);
 	input 	[WIDTH - 1:0] 	a;
 	input 	[WIDTH - 1:0] 	b;
-	input 		ctrl_s;
+	input 	[WIDTH - 1:0]	c;
+	input 	[WIDTH - 1:0] 	d;
+	input 	[1:0]	ctrl_s;
 
-	output 	[WIDTH - 1:0]	dout;
+	output 	reg 	[WIDTH - 1:0]	dout;
 
-	assign dout = ctrl_s? b: a;
+	always @ ( * ) begin
+		case (ctrl_s)
+			2'b00: dout = a;
+			2'b01: dout = b;
+			2'b10: dout = c;
+			2'b11: dout = d;
+		endcase
+	end
+
 
 endmodule // Multiplexer;
